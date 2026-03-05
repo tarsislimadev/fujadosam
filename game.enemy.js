902ec176859d4ac40ae3c8eb74921f6ad69a8235
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-05 14:09:14 +0000

    Fix enemy positions

--- game.enemy.js
+++ game.enemy.js
@@ -3,11 +3,13 @@ import * as THREE from 'three';
 export class GameEnemy extends THREE.Mesh {
   constructor() {
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
     super(geometry, material);
+    this.position.x = Math.floor(Math.random() * 21) - 10 // {0..20} - 10 = {-10..10}
+    this.position.z = 10
   }
 
   move() {
-    this.position.z -= 1e-2
+    this.position.z -= 1
   }
 }
