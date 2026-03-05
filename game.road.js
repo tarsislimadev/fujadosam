2acf092799c53faffc935a46b0d1ade6b5554936
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-03-05 13:36:26 +0000

    Fix game's road rotation and position

--- game.road.js
+++ game.road.js
@@ -9,18 +9,24 @@ export class GameRoad extends THREE.Group {
   }
 
   createFloor() {
-    return this.createPlaneGeometry(1, 10)
+    const floor = this.createPlaneGeometry(20, 100, 0xff0000)
+    floor.rotation.x = Math.PI / 2
+    return floor
   }
 
   createLeftWall() {
-    const left = this.createPlaneGeometry(1, 10)
-    left.rotation.x = Math.PI
+    const left = this.createPlaneGeometry(1, 100, 0xff9900)
+    left.position.x = -10
+    left.rotation.x = Math.PI / 2
+    left.rotation.y = Math.PI / 2
     return left
   }
 
   createRightWall() {
-    const right = this.createPlaneGeometry(1, 10)
-    right.rotation.y = Math.PI
+    const right = this.createPlaneGeometry(1, 100, 0xff9900)
+    right.position.x = 10
+    right.rotation.y = Math.PI / 2
+    right.rotation.z = Math.PI / 2
     return right
   }
 
